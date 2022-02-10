089593f5a934e59b9f242684a6eaaac77f38d720
daca <david.cardoner@redstor.com>
2022-02-10 22:14:57 +0100

    Test github actions

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
+# This is a test. Pasqui is awesome !!
 
 
 app = FastAPI(title="Predicting Wine Class with batching")
